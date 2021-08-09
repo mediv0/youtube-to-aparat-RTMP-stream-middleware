165ed90a082bc8c18e083dddc14d56d2112918a8
mediv0 <anonymousaxis@gmail.com>
2021-08-09 19:58:46 +0430

    fix: FFmpeg conversion error: At least one output file must be specified

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { exec, spawn } = require("child_process");
+const kill = require("tree-kill");
 const execSh = require("exec-sh");
 const http = require("http");
 exec("chmod +777 ./assets/youtube-dl");
@@ -11,22 +12,25 @@ const app = http.createServer(function (req, res) {
 const RTMP = "rtmp://rtmp.cdn.asset.aparat.com:443/event";
 const KEY = process.env.KEY;
 const URL = process.env.URL;
+const YOUTUBEURL = process.env.YOUTUBE;
 
 const getNewUrl = (cb) => {
     exec("./youtube-dl -g https://www.youtube.com/watch?v=21qNxnCS8WU", (error, stdout, stderr) => {
-        cb(stdout);
+        cb(stdout.trim());
     });
 };
 
 const run = (streamUrl) => {
-    const CMD = `ffmpeg -re -i ${streamUrl} -c copy -bsf:a aac_adtstoasc -preset:v ultrafast -f flv ${RTMP}/${KEY}`;
-    const activeProcess = execSh([CMD], true, (err, stdout, stderr) => {
-        if (err) {
-            activeProcess.kill();
-            getNewUrl(run);
-        }
-        console.log("stdout: ", stdout);
-        console.log("stderr: ", stderr);
+    const CMD = `ffmpeg -re -i ${streamUrl} -c:v copy -c:a copy -bsf:a aac_adtstoasc -preset:v ultrafast -f flv ${RTMP}/${KEY}`;
+    const process = execSh([CMD], true, (err, stdout, stderr) => {
+        console.log("==============================================");
+        console.log("reconnecting.........");
+        console.log("==============================================");
+        kill(process.pid);
+        getNewUrl(run);
+        console.log("==============================================");
+        console.log(stderr);
+        console.log("==============================================");
     });
 };
 
