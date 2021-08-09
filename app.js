557faf8fe534bf9ad538e31ac623848e82a10d36
mediv0 <anonymousaxis@gmail.com>
2021-08-09 13:25:49 +0430

    feat: youtube-dl binary

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
+const { exec, spawn } = require("child_process");
 const execSh = require("exec-sh");
 const http = require("http");
+exec("chmod +777 ./assets/youtube-dl");
 
-/*
-    Use this to ping the server.
-*/
 const app = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hi :)\n");
 });
 
-// Aparat RTMP stream info 
 const RTMP = "rtmp://rtmp.cdn.asset.aparat.com:443/event";
 const KEY = process.env.KEY;
 const URL = process.env.URL;
 
-// FFMPEG command - forwards hsl file to Aparat's RTMP server - optimized for free server ( -c copy )
-const CMD = `ffmpeg -re -i ${URL} -c copy -bsf:a aac_adtstoasc -preset:v ultrafast -f flv ${RTMP}/${KEY}`;
+const getNewUrl = (cb) => {
+    exec("./youtube-dl -g https://www.youtube.com/watch?v=21qNxnCS8WU", (error, stdout, stderr) => {
+        cb(stdout);
+    });
+};
 
-// Run the command
-execSh([CMD], true, (err) => {
-    console.log(err);
-});
+const run = (streamUrl) => {
+    const CMD = `ffmpeg -re -i ${streamUrl} -c copy -bsf:a aac_adtstoasc -preset:v ultrafast -f flv ${RTMP}/${KEY}`;
+    const activeProcess = execSh([CMD], true, (err, stdout, stderr) => {
+        if (err) {
+            activeProcess.kill();
+            getNewUrl(run);
+        }
+        console.log("stdout: ", stdout);
+        console.log("stderr: ", stderr);
+    });
+};
+
+run(URL);
 
 const listener = app.listen(process.env.PORT, () => {
     console.log("Your app is listening on port " + listener.address().port);
