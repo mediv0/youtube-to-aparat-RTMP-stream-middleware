390a27ddd561599ee6e97d9b26c690972e79a9fd
mediv0 <36822136+mediv0@users.noreply.github.com>
2021-10-30 17:23:29 +0330

    chore; change chmod path for youtube-dl

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { exec, spawn } = require("child_process");
 const kill = require("tree-kill");
 const execSh = require("exec-sh");
 const http = require("http");
-exec("chmod +777 ./assets/youtube-dl");
+exec("chmod +777 ./youtube-dl");
 
 const app = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
